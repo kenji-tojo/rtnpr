ee1a09e98dbcc7ab6159c624c430cab7298cf923
kenji-tojo <kenjitojo@icloud.com>
2023-01-29 00:42:29 +0900

    add disc sampler

--- src/sampler.hpp
+++ src/sampler.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <tuple>
 #include <random>
+
+#include "rtnpr_math.hpp"
 
 namespace rtnpr {
 
@@ -13,11 +16,29 @@ public:
     }
 
     T sample() { return dis(gen); }
-
 private:
     std::random_device rd;
     std::mt19937 gen;
     std::uniform_real_distribution<T> dis;
+};
+
+template<typename T>
+class UniformDiscSampler {
+public:
+    UniformDiscSampler()
+    {
+        static_assert(std::is_floating_point_v<T>);
+    }
+
+    std::pair<T,T> sample()
+    {
+        using namespace std;
+        T theta = sampler.sample() * 2. * M_PI;
+        T r = sqrt(sampler.sample());
+        return make_pair(r*cos(theta), r*sin(theta));
+    }
+private:
+    UniformSampler<T> sampler;
 };
 
 } // namespace rtnpr
